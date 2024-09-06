300775e1ee3ed157559b3ef46805858649dece24
ABRAR <Abrar-22-@hotmail.com>
2024-09-06 18:58:25 +0300

    code container

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,14 @@
-console.log("This is a popup!")
+var toggleSwitch = document.getElementById('toggleSwitch');
+
+document.getElementById('toggleBtn').addEventListener('click', function () {
+    if (toggleSwitch.style.backgroundColor === 'rgb(0, 255, 0)') {
+        toggleSwitch.style.backgroundColor = '#FF0000'; // Red color
+        // Add code to disable your extension here
+    } else {
+        toggleSwitch.style.backgroundColor = '#00FF00'; // Green color
+        // Add code to enable your extension here
+    }
+});
+
+// Set initial state to enabled
+toggleSwitch.style.backgroundColor = '#00FF00'; // Green color
